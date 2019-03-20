3f4096ecfded2cb5d6374ff506e39cea94c92d6b
Michael Yeates <michaeljyeates@gmail.com>
2019-03-20 13:38:10 +0000

    Add example config

---
+++ claim-script/example.config.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        name: 'eos',
+        api_url: 'https://eu.eosdac.io',
+        claim_key: '5....',
+        producer_name: 'eosdacserver',
+        claim_permission: 'claim'
+    }
+]
